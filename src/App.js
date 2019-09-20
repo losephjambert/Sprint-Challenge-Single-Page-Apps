baa68f0899b291cc0c2a2d6f0fc1900b1d3a47fd
Joe <josephc.lambert@gmail.com>
2019-09-20 09:25:52 -0700

    add nav links to Header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Header from './components/Header.js';
+import { Switch, Route } from 'react-router-dom';
+
 import CharactersContainer from './components/CharactersContainer';
 
 export default function App() {
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
-    <header className="ui centered">
-      <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
+    <header className='ui centered'>
+      <nav>
+        <h1 className='ui center'>
+          <Link to='/'>Rick &amp; Morty Fan Page</Link>
+        </h1>
+        <Link to='/characters'>Character List</Link>
+      </nav>
     </header>
   );
 }
