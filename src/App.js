79beb6c0324031043f5ba7fe5686c1b585935f0d
Nattajohn Rojanasupya <n.rojanasupya@gmail.com>
2019-10-18 08:18:20 -0700

    added route to welcome page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { Route } from 'react-router-dom';
+import WelcomePage from "./components/WelcomePage";
 import Header from "./components/Header.js";
 
 
 export default function App() {
-  return (
-    <main>
-      <Header />
-    </main>
-  );
+    return (
+        <main>
+        <Header />
+
+        <Route to='/' component={WelcomePage} />
+        </main>
+    );
 }
